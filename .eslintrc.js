dead67952c1ccb33a04b253d548e2966745e39fb
Amin NAIRI <aminnairi@noreply.users.github.com>
2021-02-20 15:26:56 +0100

    :tada: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    overrides: [
+        {
+            files: [".eslintrc.js", "tailwind.config.js"],
+            env: {
+                browser: true,
+                es2021: true,
+                node: true
+            }
+        }
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
