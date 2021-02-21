018baf9843ba7c9f5e4e3e6fc47e1d5a54f6ade0
Amin NAIRI <aminnairi@noreply.users.github.com>
2021-02-21 13:03:44 +0100

    :sparkles: new method to get the value or a fallback

--- sources/result.mjs
+++ sources/result.mjs
@@ -1,11 +1,13 @@
 export const error = (value) => ({
     andThen: () => error(value),
     whenError: callback => (callback(value), error(value)),
-    whenOk: () => error(value)
+    whenOk: () => error(value),
+    withDefault: (fallback) => fallback
 });
 
 export const ok = value => ({
     andThen: update => update(value),
     whenOk: callback => (callback(value), ok(value)),
-    whenError: () => ok(value)
+    whenError: () => ok(value),
+    withDefault: () => value
 });
