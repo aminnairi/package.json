25a2332943efaf0f82d10b52c1fc33969e1b0a9d
Amin NAIRI <aminnairi@noreply.users.github.com>
2021-02-21 11:26:13 +0100

    :sparkles: more checking for the current api implementation

--- sources/clipboard.mjs
+++ sources/clipboard.mjs
@@ -5,10 +5,24 @@
  */
 export const copy = text => {
     return new Promise((resolve, reject) => {
-        if (typeof text !== "string") {
-            return reject(new Error(`text is not a stringin copy(text), received ${JSON.stringify(text)}`));
+        if (!navigator) {
+            reject(new Error("Current context is not a browser"));
         }
 
-        navigator.clipboard.writeText(text).then(resolve).catch(reject);
+        if (!navigator.clipboard || !navigator.clipboard || typeof navigator.clipboard.writeText !== "function") {
+            reject(new Error("Current context does not support the clipboard API"));
+        }
+
+        if (typeof text !== "string") {
+            return reject(new Error(`text is not a string in copy(text), received ${JSON.stringify(text)}`));
+        }
+
+        const promise = navigator.clipboard.writeText(text);
+
+        if (promise instanceof Promise) {
+            return promise.then(resolve).catch(reject);
+        }
+        
+        reject(new Error("Current context does not implement the clipboard API correctly"));
     });
 };
