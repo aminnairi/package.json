7a345b99817a3d2e24fb84c7c3b73031623575d7
Amin NAIRI <aminnairi@noreply.users.github.com>
2021-02-22 08:58:37 +0100

    :white_check_mark: added more tests

--- test/functional.spec.mjs
+++ test/functional.spec.mjs
@@ -1,10 +1,26 @@
 import {describe, it} from "mocha";
 import chai from "chai";
+import {curry} from "../sources/javascript/functional.mjs";
 
 const {expect} = chai;
 
 describe("curry", () => {
-    it("should curry the function", () => {
-        
+    const add = curry((x, y) => x + y);
+
+    it("should return a function", () => {
+        expect(add).to.be.a("function");
+    });
+
+    it("should return a function that return a function when providing not enough arguments", () => {
+        expect(add(1)).to.be.a("function");
+    });
+
+    it("should allow creating functions that can return the result of the curried function", () => {
+        const increment = add(1);
+        expect(increment(2)).to.equal(3);
+    });
+
+    it("should return the result of the function when providing enough arguments", () => {
+        expect(add(1, 2)).to.equal(3);
     });
 });
