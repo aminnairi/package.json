c9cb615319b948f0d6b8f89d5f070ecd3b19f218
Amin NAIRI <aminnairi@noreply.users.github.com>
2021-02-24 13:49:00 +0100

    :tada: initial commit

---
+++ test/type.spec.mjs
@@ -0,0 +1,45 @@
+import {describe, it} from "mocha";
+import chai from "chai";
+import {typeOf} from "../sources/javascript/type.mjs";
+
+const {expect} = chai;
+
+describe("type", () => {
+    describe("typeOf", () => {
+        it("should return the type for a number", () => {
+            expect(typeOf(123)).to.equal("Number");
+        });
+
+        it("should return the type for a string", () => {
+            expect(typeOf("string")).to.equal("String");
+        });
+
+        it("should return the type for an object", () => {
+            expect(typeOf({})).to.equal("Object");
+        });
+
+        it("should return the type for an array", () => {
+            expect(typeOf([])).to.equal("Array");
+        });
+
+        it("should return the type for a synchronous function", () => {
+            expect(typeOf(() => {})).to.equal("Function");
+        });
+
+        it("should return the type for an asynchronous function", () => {
+            expect(typeOf(async () => {})).to.equal("AsyncFunction");
+        });
+
+        it("should return the type for a symbol", () => {
+            expect(typeOf(Symbol("symbol"))).to.equal("Symbol");
+        });
+
+        it("should return the type for undefined", () => {
+            expect(typeOf(undefined)).to.equal("Undefined");
+        });
+
+        it("should return the type for Null", () => {
+            expect(typeOf(null)).to.equal("Null");
+        });
+    });
+});
